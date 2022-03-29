1ed1db4f03e75cd8c768e9782ec041c4a1f884ca
Attila Dobi <17800+adobi@users.noreply.github.com>
2022-03-29 19:39:41 +0300

    Update masked-dump.php

--- config/masked-dump.php
+++ config/masked-dump.php
@@ -9,16 +9,5 @@ return [
      * Use this dump schema definition to remove, replace or mask certain parts of your database tables.
      */
     'default' => DumpSchema::define()
-        ->allTables()
-        ->table('users', function (TableDefinition $table) {
-            $table->replace('name', function (Faker $faker) {
-                return $faker->name;
-            });
-            $table->replace('email', function (Faker $faker) {
-                return $faker->safeEmail;
-            });
-            $table->mask('password');
-        })
-        ->schemaOnly('failed_jobs')
-        ->schemaOnly('password_resets'),
+        ->allTables(),
 ];
